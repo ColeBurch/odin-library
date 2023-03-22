f6ff48aa0cccbaa250def07e4ab9867268f12f91
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2023-03-22 16:30:11 -0600

    Create displayBooks function and style new divs

--- script.js
+++ script.js
@@ -14,6 +14,49 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
+function displayBooks() {
+  cardBox.innerHTML = "";
+  for (let i = 0; i < myLibrary.length; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("bookCard");
+    newDiv.id = i;
+    cardBox.appendChild(newDiv);
+    const newTitle = document.createElement("div");
+    newTitle.classList.add("bookCardText");
+    newTitle.textContent = myLibrary[i].title;
+    newDiv.appendChild(newTitle);
+    const newAuthor = document.createElement("div");
+    newAuthor.classList.add("bookCardText");
+    newAuthor.textContent = myLibrary[i].author;
+    newDiv.appendChild(newAuthor);
+    const newPages = document.createElement("div");
+    newPages.classList.add("bookCardText");
+    newPages.textContent = myLibrary[i].pages + " pages";
+    newDiv.appendChild(newPages);
+  }
+}
+
+const cardBox = document.querySelector(".cardBox");
+
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "read");
+const harryPotterAndTheSorcerersStone = new Book(
+  "Harry Potter and the Sorcerer's Stone",
+  "J.K. Rowling",
+  309,
+  "not read"
+);
+const warAndPeace = new Book("War and Peace", "Leo Tolstoy", 1215, "not read");
+const crimeAndPunishment = new Book(
+  "Crime and Punishment",
+  "Fyodor Dostoevsky",
+  430,
+  "read"
+);
 addBookToLibrary(theHobbit);
+addBookToLibrary(harryPotterAndTheSorcerersStone);
+addBookToLibrary(warAndPeace);
+addBookToLibrary(crimeAndPunishment);
 console.log(myLibrary[0].info());
+console.log(myLibrary[1].info());
+console.log(myLibrary[2].info());
+displayBooks();
